7c5902d88ac8f9122ac0ae3d77653c2a9051d74a
Edson Júnior <74505541+edsonjrs33@users.noreply.github.com>
2020-11-15 22:20:23 -0300

    Update Assassino.java

--- src/rpg/poo/model/Assassino.java
+++ src/rpg/poo/model/Assassino.java
@@ -9,14 +9,14 @@ public class Assassino extends Raca implements Combate, Constantes{
     
     public Assasino()
     {
-        super.getAtributo("Energia");
-        super.getVida(200);
-        super.getResistenciaMagica(15);
-        super.getArmadura(20);
-        super.getTenacidade(10);
-        super.getDanoDeHabilidade(40);
-        super.getPoderDeHabilidade(10);
-        super.getVelocidadeDeAtaque(20);
+        super.setAtributo("Energia");
+        super.setVida(200);
+        super.setResistenciaMagica(15);
+        super.setArmadura(20);
+        super.setTenacidade(10);
+        super.setDanoDeHabilidade(40);
+        super.setPoderDeHabilidade(10);
+        super.setVelocidadeDeAtaque(20);
         this.laminaNoturna = 45;
         this.retaDaMorte = 35;
         this.energia = 120;
