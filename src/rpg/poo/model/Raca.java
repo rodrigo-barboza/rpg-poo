c1ab274903fccc6619608e314f4c795dbfb54a89
JP-BRITES <jpbrites@github.com>
2020-11-15 22:18:17 -0300

    Criação da classe Raca

--- src/rpg/poo/model/Raca.java
+++ src/rpg/poo/model/Raca.java
@@ -1,14 +1,83 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rpg.poo.model;
 
-/**
- *
- * @author Rodrigo
- */
 public abstract class Raca extends Personagem{
-    
+        //Atributos da classe Raca
+        private String atributo;
+        private int vida;
+        private int resistenciaMagica;
+        private int armadura;
+        private int tenacidade;
+        private int danoDeHabilidade;
+        private int poderDeHabilidade;
+        private int velocidadeDeAtaque;
+
+
+    //Getters e Setters dos atributos
+    public String getAtributo() {
+        return atributo;
+    }
+
+    public void setAtributo(String atributo) {
+        this.atributo = atributo;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getResistenciaMagica() {
+        return resistenciaMagica;
+    }
+
+    public void setResistenciaMagica(int resistenciaMagica) {
+        this.resistenciaMagica = resistenciaMagica;
+    }
+
+    public int getArmadura() {
+        return armadura;
+    }
+
+    public void setArmadura(int armadura) {
+        this.armadura = armadura;
+    }
+
+    public int getTenacidade() {
+        return tenacidade;
+    }
+
+    public void setTenacidade(int tenacidade) {
+        this.tenacidade = tenacidade;
+    }
+
+    public int getDanoDeHabilidade() {
+        return danoDeHabilidade;
+    }
+
+    public void setDanoDeHabilidade(int danoDeHabilidade) {
+        this.danoDeHabilidade = danoDeHabilidade;
+    }
+
+    public int getPoderDeHabilidade() {
+        return poderDeHabilidade;
+    }
+
+    public void setPoderDeHabilidade(int poderDeHabilidade) {
+        this.poderDeHabilidade = poderDeHabilidade;
+    }
+
+    public int getVelocidadeDeAtaque() {
+        return velocidadeDeAtaque;
+    }
+
+    public void setVelocidadeDeAtaque(int velocidadeDeAtaque) {
+        this.velocidadeDeAtaque = velocidadeDeAtaque;
+    }
+        
+        
+        
 }
